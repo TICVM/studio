d52b7868d30ff452001952c143514b663f1f13f1
CVM Aulas online <cvmspinfo@gmail.com>
2026-02-27 17:27:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3872796415-1fc4f",
+  "appId": "1:463587870934:web:42eea04388505a2b0c6177",
+  "apiKey": "AIzaSyBnGVgiPQnYawmkPbnN3ac_BD8FeXne3XQ",
+  "authDomain": "studio-3872796415-1fc4f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "463587870934"
+};
